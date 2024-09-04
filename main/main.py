eb787742b4b4a22c1af4968b27f52713b496e0ba
Lucas Carneiro do Ouro <lusca0320@gmail.com>
2024-09-04 11:30:32 -0300

    Modificações para rodar em prod

--- main/main.py
+++ main/main.py
@@ -239,4 +239,4 @@ def atualizar_dados():
     return jsonify({'message': 'Método não permitido.'}), 405
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
